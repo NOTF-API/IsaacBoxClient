833f56e7b26b0f5e3e9f29410e975c1202b0e5b0
NOTF-API <notf@qq.com>
2023-11-12 18:24:38 +0800

    更新发生错误时的提示内容

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const createMainWindow = () => {
   win = new BrowserWindow({
     width: 800,
     height: 600 + 28,
-    minWidth:800,
+    minWidth: 800,
     minHeight: 600 + 28,
     // resizable: false,
     show: false,
@@ -27,7 +27,7 @@ const createMainWindow = () => {
     }
   })
   win.menuBarVisible = false
-//   win.webContents.openDevTools() // 打开electron控制台
+  //   win.webContents.openDevTools() // 打开electron控制台
   win.loadFile(path.join(__dirname, "dist", "index.html"))
   win.on("ready-to-show", () => {
     win.show();
@@ -72,7 +72,9 @@ const isPortTaken = (port) => {
   catch (e) {
     dialog.showMessageBoxSync({
       title: "错误",
-      message: `无法获取游戏目录，请检查IsaacBoxUtility.exe是否被杀毒软件删除或限制访问`,
+      message: `无法获取游戏目录，请检查IsaacBoxUtility.exe是否被杀毒软件删除或限制访问。
+      您可以手动复制dependencies文件夹下的isaac_box与isaac_socket目录到游戏MOD目录，然后重新启动游戏.
+      请确保游戏内只有一个IsaacBox和IsaacSocket MOD,否则无法正常运行!`,
       type: "error"
     })
     return;
@@ -84,7 +86,9 @@ const isPortTaken = (port) => {
   } catch (e) {
     dialog.showMessageBoxSync({
       title: "错误",
-      message: `无法获取MOD信息`,
+      message: `无法获取所有MOD信息.
+      您可以手动复制dependencies文件夹下的isaac_box与isaac_socket目录到游戏MOD目录,
+      然后重新启动游戏.请确保游戏内只有一个IsaacBox和IsaacSocket MOD,否则无法正常运行!`,
       type: "error"
     })
     return;
@@ -96,7 +100,8 @@ const isPortTaken = (port) => {
   } catch (e) {
     dialog.showMessageBoxSync({
       title: "错误",
-      message: `无法获取所需MOD信息`,
+      message: `无法读取您是否安装了必须的MOD，您可以手动复制dependencies文件夹下的isaac_box与isaac_socket目录到游戏MOD目录,
+      然后重新启动游戏.请确保游戏内只有一个IsaacBox和IsaacSocket MOD,否则无法正常运行!`,
       type: "error"
     })
     return;
@@ -116,7 +121,8 @@ const isPortTaken = (port) => {
   } catch (e) {
     dialog.showMessageBoxSync({
       title: "错误",
-      message: `无法安装所需MOD`,
+      message: `无法安装所需MOD.您可以手动复制dependencies文件夹下的isaac_box与isaac_socket目录到游戏MOD目录,
+      然后重新启动游戏.请确保游戏内只有一个IsaacBox和IsaacSocket MOD,否则无法正常运行!`,
       type: "error"
     })
     return;
@@ -128,7 +134,7 @@ const isPortTaken = (port) => {
   catch (e) {
     dialog.showMessageBoxSync({
       title: "错误",
-      message: `无法打开IsaacSocket连接工具，请检查.NET环境是否安装，IsaacSocketUtility.exe是否被杀毒软件删除或限制访问，IsaacBoxUtility.exe,或者请以管理员权限打开。`,
+      message: `无法打开IsaacSocket连接工具，请检查.NET环境是否安装或IsaacSocketUtility.exe是否被杀毒软件删除或限制访问,或者请以管理员权限打开。如果无法解决，请手动开启dependencies/IsaacSocketUtility/IsaacSocket.exe 并在游戏运行时保持开启状态。`,
       type: "error"
     })
     return;
